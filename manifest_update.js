8ccc74b47892cd2e41c3f3e68ca73a13a4c4ad68
Scott Deakin <sdeaks@gmail.com>
2016-08-19 15:47:04 +0100

    More readable manifest_update.js

--- manifest_update.js
+++ manifest_update.js
@@ -6,13 +6,15 @@ var logger = require('winston');
 logger.level = 'debug';
 
 var promisify = require('promisify-node');
+var thunkify = require('thunkify');
 var fs = promisify('fs');
 var request = promisify('request');
 var JSZip = require('jszip');
 var path = require('path');
-var mkdirp = promisify('mkdirp');
+var mkdirp = require('mkdirp');
 var ps = require('promise-streams');
 var sqlite3 = require('sqlite3').verbose();
+var MongoClient = require('mongodb').MongoClient;
 
 // fudge whilst we are running this directly...
 var config = require('./config');
@@ -20,99 +22,145 @@ module.exports.config = config;
 
 var api = require('./bot/modules/destiny/api');
 
+var sdb;  // sqlite3 handle
+var mdb;  // mongodb handle
+var lang;  // global lang 
+
+var mongoUrl = 'mongodb://localhost:27017/destiny';
+
 function processTable(table) {
-    logger.debug("Processing table: %s", table.name);
-    //return Promise.resolve();
-}
 
+    return co(function* () {
+        logger.debug("Processing table: %s", table.name);
 
-function processDb(dbfile) {
-    logger.debug("loading DB: %s", dbfile);
-    var db;
-    // we are not able to promisify the DB constructor!!! <sigh>
-    return new Promise(function (resolve, reject) {
-        db = new sqlite3.Database(dbfile, sqlite3.OPEN_READONLY, function (err) {
-            if (err) return reject(err);
-            return resolve();
-        });
-    }).then(function () {
-        logger.debug("DB opened successfully");
-        // loop through the tables
-        return new Promise(function (resolve, reject) {
-            db.each("select name from sqlite_master where type='table'", function (err, table) {
+        var rows = yield new Promise(function (resolve, reject) {
 
-                // does our error checking go out the window here?
+            sdb.all("select * from " + table.name, function (err, rows) {
                 if (err) return reject(err);
-
-                processTable(table);
-            }, function (err, count) {
-                if (err) return reject(err);
-                resolve(count);
+                resolve(rows);
             });
         });
 
+        var json;
 
-    }).then(function (count) {
-        logger.debug("finished processing %s tables", count);
-        logger.debug("closing DB");
-        return db.close();
-    });;
+        for (var r = 0; r < rows.length; r++) {
+
+            json = JSON.parse(rows[r].json);
+
+            json._id = json.hash || json.statId || rows[r].id;
+
+            if (!json._id && json._id !== 0) {
+                logger.error("no id found in table: %s\nid: %s\njson: %s", table.name, rows[r].id, rows[r].json);
+                //process.exit();
+                return;
+            }
+
+            yield new Promise(function (resolve, reject) {
+                mdb.collection('destiny.manifest.' + lang + '.' + table.name).replaceOne(
+                    { "_id": json._id }, json, { upsert: true },
+                    function (err) {
+                        if (err) return reject(err)
+                        resolve();
+                    }
+                );
+
+            });
+
+        }
+
+        logger.debug("processed %s rows", rows.length);
+
+    });
+
 
 }
 
 
-function update(lang) {
+function update() {
 
-    // because we are using promises, we have issues with scope
-    // so here are the variables we need between .then()s
+    return co(function * () {
 
-    var dbfile;
-    var zipobj;
-    var tmpdir = path.join(config.appDir, 'tmp', 'destiny');
-    var db;
+        var tmpdir = path.join(config.appDir, 'tmp', 'destiny');
+        var manifest = yield api.manifest();
 
-    return api.manifest().
-    then(function(manifest) {
-       if(!manifest.mobileWorldContentPaths[lang]) {
+        if(!manifest.mobileWorldContentPaths[lang]) {
             logger.error("Cannot find manifest for lang: %s", lang);
             return;
         }
         var mwcUrl = 'http://www.bungie.net' + manifest.mobileWorldContentPaths[lang];
         logger.debug("Retrieving Mobile World Contents from %s", mwcUrl);
-        return request.get({ url: mwcUrl, encoding: null });
-    }).then(function (res, body) {
+        var res = yield request.get({ url: mwcUrl, encoding: null });
+
         if (res.statusCode !== 200) {
             logger.error("failed to download mobileWorldContent: %s\n", res.statusCode);
             throw new Error("download failure: " + res.statusMessage);
-        } else {
-            logger.debug("Unzipping manifest file...");
-            var zip = new JSZip();
-            return zip.loadAsync(res.body);
-        }
-    }).then(function (zip) {
+        } 
+        logger.debug("Unzipping manifest file...");
+        var zip = new JSZip();
+        yield zip.loadAsync(res.body);
+
         var files = zip.file(/.content$/);
         // there should only be one file
-        if(files.length > 1) throw new Error("More than one DB in manifest ZIP");
-        zipobj = files.shift();
-        dbfile = path.join(tmpdir, zipobj.name);
+        if (files.length > 1) throw new Error("More than one DB in manifest ZIP");
+        var zipobj = files.shift();
+        var dbfile = path.join(tmpdir, zipobj.name);
         logger.debug("creating dir: %s", tmpdir);
-        return mkdirp(tmpdir);
-    }).then(function () {
+        mkdirp.sync(tmpdir);
+
         logger.debug("extracting file: %s, to: %s", zipobj.name, dbfile);
-        return ps.wait(zipobj.
+        // wait until the file has finished unzipping
+        yield ps.wait(zipobj.
                 nodeStream().
                 pipe(fs.createWriteStream(dbfile)));
-    }).then(function () {
-        return processDb(dbfile);
+
+
+        logger.debug("loading DB: %s", dbfile);
+        // we are not able to promisify the DB constructor!!! <sigh>
+        yield new Promise(function (resolve, reject) {
+            sdb = new sqlite3.Database(dbfile, sqlite3.OPEN_READONLY, function (err) {
+                if (err) return reject(err);
+                return resolve();
+            });
+        });
+
+        logger.debug("SQLite DB opened successfully");
+
+        mdb = yield new Promise(function (resolve, reject) {
+            MongoClient.connect(mongoUrl, function (err, res) {
+                if(err) return reject(err);
+                resolve(res);
+            });
+        });
+
+        logger.debug("Connected to mongoDB");
+
+        var tables = yield new Promise(function (resolve, reject) {
+            sdb.all("select name from sqlite_master where type='table'", function (err, tables) {
+
+                // does our error checking go out the window here?
+                if (err) return reject(err);
+                resolve(tables);
+            });
+        });
+
+        for(var t = 0; t < tables.length; t++) {
+            yield processTable(tables[t]);
+        }
+
     });
 
 }
 
 
 if(process.argv.length > 2) {
-    update(process.argv[2]).
+    lang = process.argv[2];
+    update().
         then(function () {
             console.log("finished updating manifest");
+            sdb.close();
+            mdb.close();
+        }).catch(function (err) {
+            console.err(err);
         });
 } else {
     console.log('Usage: manifest_update [lang]');
