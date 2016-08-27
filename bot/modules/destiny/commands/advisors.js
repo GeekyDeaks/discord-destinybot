4ae1ad020f9975e21718337569f5402b28b746ce
Phillip Jackson <unisys12@gmail.com>
2016-08-27 12:07:02 -0500

    add vars to parse advisor response body

--- bot/modules/destiny/commands/advisors.js
+++ bot/modules/destiny/commands/advisors.js
@@ -15,23 +15,140 @@ function exec(cmd) {
 
     return co(function *() {
 
-        var msg = cmd.msg;
+        var msg = cmd.msg; 
+        var input = cmd.args[0];
+        //var activity = input.replace(/\s+/g, "");
         var busyMsg;
-        var daily = {};
-        var weekly = {};
 
-        busyMsg = yield bot.sendMessage(msg, "Pulling latest Destiny Daily and Weekly Advisors"+"** :mag:");
-
+        // Retrieve latest Advisor Data
         var advisor = yield api.advisor();
-        var activities = advisor.data.activities;
-        var activityName = advisor.data.activities.display;
-        var categories = advisor.data.activityCategories;
 
+        /**
+         * Base Response property
+         * - data
+         * - definitions
+         */
+        var res = [advisor].Response;
 
-        for (let i in activityName) {
-            console.log(i);
-        }
+        /**
+         * == Response.data ==
+         *
+         * Contains : 
+         * - activities, 
+         * - activityCategories
+         */
+        var resData = [res].data;
 
+        /**
+         * === Response.data.activities ===
+         * 
+         * Returns a list of all known the activities
+         * - prisonofelders
+         * - elderchallenge
+         * - trials
+         * - armsday
+         * - weeklycrucible
+         * - kingsfall
+         * - vaultofglass
+         * - crota
+         * - nightfall
+         * - heroicstrike
+         * - dailychapter
+         * - dailycrucible
+         * - prisonofelders-playlist
+         * - ironbanner
+         * - xur
+         * - srl
+         * 
+         * Extending this to include one of the above activities
+         * will result in the following properties being exposed:
+         * - identifier
+         * - status
+         * - display
+         * - vendorHash
+         * - bountyHashes
+         * - extended
+         */
+        var activities = [resData].activities;
+
+        /**
+         * === Response.data.activityCategories ===
+         * 
+         * This exposes a list of known category hashes. These do 
+         * not appear anywhere else in the object and would need to 
+         * referenced from the "DestinyActivityCategoryDefinition" 
+         * collection of the database.
+         * 
+         * An example query to do this would look like:
+         * db.getCollection(
+         *      'destiny.manifest.en.DestinyActivityCategoryDefinition'
+         * ).find({ hash: hash});
+         */
+        var activityCategories = [resData].activityCategories;
+
+        /**
+         * == Response.definitions ==
+         * 
+         * This is only returned and part of the response body if the
+         * query string, ?definitions=true, is passed when making the 
+         * API call to the advisor URI. 
+         * 
+         * It contains a large list of hash look-ups, that you would 
+         * otherwise have to make a database call to cross-reference.
+         * 
+         * - items
+         * - buckets
+         * - stats
+         * - perks
+         * - talentGrids
+         * - statGroups
+         * - progressionMappings
+         * - itemCategories
+         * - sources
+         * - objectives
+         * - progressions
+         * - damageTypes
+         * - materialRequirements
+         * - unlockValues
+         * - vendorDetails
+         * - locations
+         * - factions
+         * - events
+         * - vendorCategories
+         * - vendorSummaries
+         * - destinations
+         * - activities
+         * - books
+         * - places
+         * - activityTypes
+         * - activityBundles
+         * - enemyRaces
+         * - flags
+         */
+        var definitions = [res].definitions;
+
+        /**
+         * === Response.definitions.destinitions ===
+         * 
+         * Contains a list of destination hash objects
+         * that contain destination info. This is used to
+         * replace the destination hash found inside the 
+         * `activities.display.destinationHash` property.
+         * 
+         * 518553403
+         * 2512542997
+         * 2777041980
+         * 2897855902
+         * 3393213630
+         * 3393905939
+         * 4072959335
+         * 4233735899
+         */
+        var destinations = [definitions].destinations;
+
+        //console.log("Activity found was " + activities[input].display.advisorTypeCategory);
+        busyMsg = yield bot.sendMessage(msg, "Pulling latest Destiny Daily and Weekly Advisors** :mag:");
+        
     })
 
 }
