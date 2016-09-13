5b946f1de3fe05ad90bb5a1addcbcf7ba9d696a2
Scott Deakin <sdeaks@gmail.com>
2016-09-13 17:56:08 +0100

    updated my command to v9.x

--- bot/modules/gamer/commands/my.js
+++ bot/modules/gamer/commands/my.js
@@ -6,6 +6,7 @@ var logger = require('winston');
 var md = require('../../../markdown');
 var moment = require('moment-timezone');
 var message = require('../../../message');
+var gamer = require('../index');
 
 var app = require.main.exports;
 var bot = app.bot;
@@ -18,36 +19,81 @@ function exec(cmd) {
 
         var msg = cmd.msg;
         var args = cmd.args;
+        var server = msg.guild || app.defaultServer;
+        var now = moment();
 
         var option = args.shift();
 
+        if(!option) {
+            // display the user?
+            var name = cmd.msg.author.username;
+            var g = yield gamer.findById(cmd.msg.author.id);
+            if (!g) {
+                return message.send(msg, "Sorry, could not find **" + md.escape(name) + "**", cmd.pm, 10000);
+            }
+
+            var toSend = ["```" + cmd.format];
+            toSend.push("Discord ID: @" + g.discord.name);
+            if (g.psn)
+                toSend.push("       PSN: " + g.psn);
+            if (g.xbl)
+                toSend.push("       XBL: " + g.xbl);
+            if (g.games)
+                toSend.push("     Games: " + g.games.join(", "));
+            // get joined at timestamp
+            var user = server.members.find("id", g.discord.id);
+            if (user) {
+                toSend.push(" Joined At: " + user.joinDate.toISOString());
+            }
+
+            if (g.tz && moment.tz.zone(g.tz)) {
+                toSend.push(" Localtime: " + now.tz(g.tz).format("HH:mm (Z z)"));
+            } else if (g.tz) {
+                toSend.push("  Timezone: " + g.tz);
+            }
+            toSend.push("```");
+
+            return message.send(msg, toSend, cmd.pm);
+
+        }
+
         switch(option.toLowerCase()) {
             case 'xbl':
-                if(!args.length) return message.send(msg, "missing xbl gamertag", cmd.pm, 10000);
+            case 'xbox':
                 yield db.collection(config.modules.gamer.collection).updateOne(
                     { "discord.id": msg.author.id },
                     { $set: { xbl: args[0], "discord.name" : msg.author.username, modified: true } },
                     { upsert: true }
                 );
-                return message.send(msg, "Updated XBL Tag to: "+args[0]);
-
+                if(args[0]) {
+                    return message.send(msg, "Updated XBL Tag to: "+args[0]);
+                } else {
+                    return message.send(msg, "Removed XBL Tag");
+                }
+                
             case 'psn':
-                if(!args.length) return message.send(msg, "missing psn id", cmd.pm, 10000);
+            case 'playstation':
                 yield db.collection(config.modules.gamer.collection).updateOne(
                     { "discord.id": msg.author.id },
                     { $set: { psn: args[0], "discord.name" : msg.author.username, modified: true } },
                     { upsert: true }
                 );
-                return message.send(msg, "Updated PSN Tag to: "+args[0]);
+                if(args[0]) {
+                    return message.send(msg, "Updated PSN ID to: "+args[0]);
+                } else {
+                    return message.send(msg, "Removed PSN ID");
+                }
 
             case 'game':
-                if(!args.length) return message.send(msg, "no games specified", cmd.pm, 10000);
+            case 'games':
+                if(!args.length) return message.send(msg, "Sorry "+ msg.author + ", there are no games specified", cmd.pm, 10000);
                 break;
             case 'tz':
-                if(!args.length) return message.send(msg, "missing tz", cmd.pm, 10000);
+            case 'timezone':
+                if(!args.length) return message.send(msg, "Sorry "+ msg.author + ", the `timezone` is missing", cmd.pm, 10000);
                 break;
             default:
-                return message.send(msg, "sorry, don't understand `"+option+"`", cmd.pm, 10000);
+                return message.send(msg, "sorry " + msg.author + ", I don't understand `"+option+"`", cmd.pm, 10000);
 
         }
         
